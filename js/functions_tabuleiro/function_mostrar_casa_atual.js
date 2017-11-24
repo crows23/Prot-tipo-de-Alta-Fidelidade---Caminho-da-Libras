8e618c80e8064ee9916db93fcca3d541b2b4280f
crows23 <33915019+crows23@users.noreply.github.com>
2017-11-24 15:19:38 -0200

    Create function_mostrar_casa_atual.js
    Adicionado a função que mostra em que casa a atual equipe está posicionada

---
+++ js/functions_tabuleiro/function_mostrar_casa_atual.js
@@ -0,0 +1,114 @@
+window.setInterval(function() {
+	equipeJogandoAgora = window.localStorage.getItem("equipeJogandoAgora");
+	mostrarCasaAtual(equipeJogandoAgora)
+}, 100);
+
+function mostrarCasaAtual(equipeJogandoAgora) {
+	var mostradorCasa = document.getElementById("mostradorCasaAtual").querySelector("img");
+	var atualEquipe = (equipeJogandoAgora-1);
+	var indiceDivAtual;
+
+	for (var i = 0; i < divs.length; i++) {
+		var figureMarginLeftSplit = figures[atualEquipe].style.marginLeft.split("px");
+		var figureMarginTopSplit = figures[atualEquipe].style.marginTop.split("px");
+
+		var divMarginLeftSplit = window.getComputedStyle(divs[i]).marginLeft.split("px");
+		var divMarginTopSplit = window.getComputedStyle(divs[i]).marginTop.split("px");
+
+		if (nEquipes == 2) {
+			if (i != 0 && i != (divs.length - 1)) {
+				if (parseInt(figureMarginLeftSplit[0]) == parseInt(divMarginLeftSplit[0]) && 
+					parseInt(figureMarginTopSplit[0]) == parseInt(divMarginTopSplit[0])
+					||
+					parseInt(figureMarginLeftSplit[0]) == (parseInt(divMarginLeftSplit[0]) + 47) && 
+					parseInt(figureMarginTopSplit[0]) == (parseInt(divMarginTopSplit[0]) + 46)) {
+
+					indiceDivAtual = i;
+					break;
+				}
+			}
+
+			else {
+				if (parseInt(figureMarginLeftSplit[0]) == parseInt(divMarginLeftSplit[0]) && 
+					parseInt(figureMarginTopSplit[0]) == parseInt(divMarginTopSplit[0])
+					||
+					parseInt(figureMarginLeftSplit[0]) == (parseInt(divMarginLeftSplit[0]) + 62) && 
+					parseInt(figureMarginTopSplit[0]) == (parseInt(divMarginTopSplit[0]) + 51)) {
+
+					indiceDivAtual = i;
+					break;
+				}
+			}
+		}
+
+		else if (nEquipes == 3) {
+			if (i != 0 && i != (divs.length - 1)) {
+				if (parseInt(figureMarginLeftSplit[0]) == parseInt(divMarginLeftSplit[0]) && 
+					parseInt(figureMarginTopSplit[0]) == parseInt(divMarginTopSplit[0])
+					||
+					parseInt(figureMarginLeftSplit[0]) == (parseInt(divMarginLeftSplit[0]) + 47) && 
+					parseInt(figureMarginTopSplit[0]) == parseInt(divMarginTopSplit[0])
+					||
+					parseInt(figureMarginLeftSplit[0]) == (parseInt(divMarginLeftSplit[0]) + 24) && 
+					parseInt(figureMarginTopSplit[0]) == (parseInt(divMarginTopSplit[0]) + 46)) {
+
+					indiceDivAtual = i;
+					break;
+				}
+			}
+			else {
+				if (parseInt(figureMarginLeftSplit[0]) == parseInt(divMarginLeftSplit[0]) && 
+					parseInt(figureMarginTopSplit[0]) == parseInt(divMarginTopSplit[0])
+					||
+					parseInt(figureMarginLeftSplit[0]) == (parseInt(divMarginLeftSplit[0]) + 62) && 
+					parseInt(figureMarginTopSplit[0]) == parseInt(divMarginTopSplit[0])
+					||
+					parseInt(figureMarginLeftSplit[0]) == (parseInt(divMarginLeftSplit[0]) + 31) && 
+					parseInt(figureMarginTopSplit[0]) == (parseInt(divMarginTopSplit[0]) + 51)) {
+
+					indiceDivAtual = i;
+					break;
+				}
+			}
+		}
+
+		else if (nEquipes == 4) {
+			if (i != 0 && i != (divs.length - 1)) {
+				if (parseInt(figureMarginLeftSplit[0]) == parseInt(divMarginLeftSplit[0]) && 
+					parseInt(figureMarginTopSplit[0]) == parseInt(divMarginTopSplit[0])
+					||
+					parseInt(figureMarginLeftSplit[0]) == (parseInt(divMarginLeftSplit[0]) + 47) && 
+					parseInt(figureMarginTopSplit[0]) == parseInt(divMarginTopSplit[0])
+					||
+					parseInt(figureMarginLeftSplit[0]) == parseInt(divMarginLeftSplit[0]) && 
+					parseInt(figureMarginTopSplit[0]) == (parseInt(divMarginTopSplit[0]) + 46)
+					||
+					parseInt(figureMarginLeftSplit[0]) == (parseInt(divMarginLeftSplit[0]) + 47) && 
+					parseInt(figureMarginTopSplit[0]) == (parseInt(divMarginTopSplit[0]) + 46)) {
+					
+					indiceDivAtual = i;
+					break;
+				}
+			}
+			else {
+				if (parseInt(figureMarginLeftSplit[0]) == parseInt(divMarginLeftSplit[0]) && 
+					parseInt(figureMarginTopSplit[0]) == parseInt(divMarginTopSplit[0])
+					||
+					parseInt(figureMarginLeftSplit[0]) == (parseInt(divMarginLeftSplit[0]) + 62) && 
+					parseInt(figureMarginTopSplit[0]) == parseInt(divMarginTopSplit[0])
+					||
+					parseInt(figureMarginLeftSplit[0]) == parseInt(divMarginLeftSplit[0]) && 
+					parseInt(figureMarginTopSplit[0]) == (parseInt(divMarginTopSplit[0]) + 51)
+					||
+					parseInt(figureMarginLeftSplit[0]) == (parseInt(divMarginLeftSplit[0]) + 62) && 
+					parseInt(figureMarginTopSplit[0]) == (parseInt(divMarginTopSplit[0]) + 51)) {
+					
+					indiceDivAtual = i;
+					break;
+				}
+			}
+		}
+	}
+
+	mostradorCasa.alt = indiceDivAtual + 1;
+}
